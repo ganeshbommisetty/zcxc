ab0e2f8448fefefe73790c4c969ba48eceaa6b36
ganesh bommisetty <ganeshbommisetty@my.unt.edu>
2023-04-13 09:16:28 -0500

    Testing project

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello intellj");
+        System.out.println("hello intsellj");
     }
 }
